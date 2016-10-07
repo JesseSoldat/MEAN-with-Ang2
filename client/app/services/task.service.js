b394f7fa2dff4204a499d4bcf5349f092cb64445
jesse soldat <jsoldat@hotmail.com>
2016-10-07 10:40:28 +0700

    client can talk to mongo lab

--- client/app/services/task.service.js
+++ client/app/services/task.service.js
@@ -9,20 +9,22 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var task_service_1 = require('./services/task.service');
-var AppComponent = (function () {
-    function AppComponent() {
+var http_1 = require('@angular/http');
+require('rxjs/add/operator/map');
+var TaskService = (function () {
+    function TaskService(http) {
+        this.http = http;
+        console.log('Task Service Initialized....');
     }
-    AppComponent = __decorate([
-        core_1.Component({
-            moduleId: module.id,
-            selector: 'my-app',
-            templateUrl: 'app.component.html',
-            providers: [task_service_1.TaskService]
-        }), 
-        __metadata('design:paramtypes', [])
-    ], AppComponent);
-    return AppComponent;
+    TaskService.prototype.getTasks = function () {
+        return this.http.get('http://localhost:3000/api/tasks')
+            .map(function (res) { return res.json(); });
+    };
+    TaskService = __decorate([
+        core_1.Injectable(), 
+        __metadata('design:paramtypes', [http_1.Http])
+    ], TaskService);
+    return TaskService;
 }());
-exports.AppComponent = AppComponent;
-//# sourceMappingURL=app.component.js.map
+exports.TaskService = TaskService;
+//# sourceMappingURL=task.service.js.map
