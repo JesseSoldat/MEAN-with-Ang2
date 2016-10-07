0163badef97ba2a5ce250044037562a8fc712d60
jesse soldat <jsoldat@hotmail.com>
2016-10-07 07:32:37 +0700

    server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var tasks = require('./routes/tasks');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
