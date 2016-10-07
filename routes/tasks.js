6c784d651cc3e9d3d2b8f7a601ddd011fdfecf15
jesse soldat <jsoldat@hotmail.com>
2016-10-07 08:36:38 +0700

    can connect to mongo lab and view tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
 
+var db = mongojs('mongodb://jesse:jesse@ds053156.mlab.com:53156/jlab', ['tasks'])
 
+//Get All Tasks
 router.get('/tasks', function(req, res, next){
-	res.send('Task Api');
+	// res.send('Task Api');
+	db.tasks.find(function(err, tasks){
+		if(err){
+			res.send(err);
+		} else {
+			res.json(tasks);
+		}
+	});
+});
+//Get a Single Task
+router.get('/task/:id', function(req, res, next){
+	db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+		if(err){
+			res.send(err);
+		} else {
+			res.json(task);
+		}
+	})
 })
-
 module.exports = router;
